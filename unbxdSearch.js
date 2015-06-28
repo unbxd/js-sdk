0dc679f0f29d36f37daebb53a2745484dbaac2f4
Rahul Chanila <rahulcssjce@gmail.com>
2015-06-28 15:45:37 +0530

    add polyfill for reduce

--- unbxdSearch.js
+++ unbxdSearch.js
@@ -22,6 +22,38 @@ var unbxdSearchInit = function(jQuery, Handlebars){
 	};
     }
 
+  // Production steps of ECMA-262, Edition 5, 15.4.4.21
+  // Reference: http://es5.github.io/#x15.4.4.21
+  if (!Array.prototype.reduce) {
+    Array.prototype.reduce = function(callback /*, initialValue*/) {
+      'use strict';
+      if (this == null) {
+	throw new TypeError('Array.prototype.reduce called on null or undefined');
+      }
+      if (typeof callback !== 'function') {
+	throw new TypeError(callback + ' is not a function');
+      }
+      var t = Object(this), len = t.length >>> 0, k = 0, value;
+      if (arguments.length == 2) {
+	value = arguments[1];
+      } else {
+	while (k < len && !(k in t)) {
+	  k++;
+	}
+	if (k >= len) {
+	  throw new TypeError('Reduce of empty array with no initial value');
+	}
+	value = t[k++];
+      }
+      for (; k < len; k++) {
+	if (k in t) {
+	  value = callback(value, t[k], k, t);
+	}
+      }
+      return value;
+    };
+  }
+
     if (!Object.keys) Object.keys = function(o) {
 	if (o !== Object(o))
             throw new TypeError('Object.keys called on a non-object');
