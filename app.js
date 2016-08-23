bedb053565fb8aaac76cc850c03b1c1898250952
santosh1994 <santosh.siddarth123@gmail.com>
2016-08-23 16:22:01 +0530

    configure karma and setup search to add testcases

---
+++ app.js
@@ -0,0 +1,158 @@
+var config = {
+  siteName : 'newtest-u1436941805532'
+  ,APIKey : 'cd97d8b7741240763b89e5c014bc3aab'
+  ,type : 'search'
+  ,getCategoryId : ''
+  ,inputSelector : '#search_input'
+  ,searchButtonSelector : '#search_button'
+  ,spellCheck : '#did_you_mean'
+  ,spellCheckTemp : 'Did you mean : {{suggestion}} ?'
+  ,searchQueryDisplay : '#search_title'
+  ,searchQueryDisplayTemp : 'Showing results for {{query}} - {{start}}-{{end}} of {{numberOfProducts}} Results'
+  ,pageSize : 12
+  ,searchResultSetTemp : [
+    '{{#products}}'
+      ,'<li>'
+        ,'<a href="product.html?pid={{uniqueId}}" id="pdt-{{uniqueId}}" class="result-item" unbxdParam_sku="{{uniqueId}}" unbxdParam_pRank="{{unbxdprank}}" unbxdAttr="product">'
+          ,'<div class="result-image-container">'
+            ,'<span class="result-image-horizontal-holder">'
+              ,'<img src="{{{image_url}}}" alt="{{{title}}}">'
+            ,'</span>'
+          ,'</div>'
+          ,'<div class="result-brand">{{{brand}}}</div>'
+          ,'<div class="result-title">{{{title}}}</div>'
+          ,'<div class="result-price">'
+            ,'${{price}}'
+          ,'</div>'
+        ,'</a>'
+      ,'</li>'
+    ,'{{/products}}'
+  ].join('')
+  ,searchResultContainer : '#results_container'
+  ,isClickNScroll: false
+  ,clickNScrollSelector : ''
+  ,isAutoScroll : true
+  ,facetTemp : [
+    '{{#facets}}'
+      ,'<div class="facet-block">'
+        ,'<h3>{{name}}</h3>'
+        ,'<div class="facet-values">'
+          ,'<ul>'
+            ,'{{#selected}}'
+              ,'<li unbxdParam_facetName="{{../facet_name}}" unbxdParam_facetValue="{{value}}">'
+                ,'<label for="{{../facet_name}}_{{value}}">'
+                  ,'<input type="checkbox" checked class="filter-checkbox" unbxdParam_facetName="{{../facet_name}}" unbxdParam_facetValue="{{value}}" id="{{../facet_name}}_{{value}}"> {{prepareFacetValue value}} ({{count}})'
+                ,'</label>'
+              ,'</li>'
+            ,'{{/selected}}'
+            ,'{{#unselected}}'
+              ,'<li unbxdParam_facetName="{{../facet_name}}" unbxdParam_facetValue="{{value}}">'
+                ,'<label for="{{../facet_name}}_{{value}}">'
+                  ,'<input type="checkbox" class="filter-checkbox" unbxdParam_facetName="{{../facet_name}}" unbxdParam_facetValue="{{value}}" id="{{../facet_name}}_{{value}}"> {{prepareFacetValue value}} ({{count}})'
+                ,'</label>'
+              ,'</li>'
+            ,'{{/unselected}}'
+          ,'</ul>'
+        ,'</div>'
+      ,'</div>'
+    ,'{{/facets}}'
+    ,'{{#rangefacets}}'
+      ,'<div class="facet-block"'
+        ,'<h3>{{name}}</h3>'
+        ,'<div class="facet-values">'
+          ,'<ul>'
+            ,'{{#selected}}'
+              ,'<li unbxdParam_facetName="{{../facet_name}}" unbxdParam_facetValue="{{value}}">'
+                ,'<label for="{{../facet_name}}_{{value}}">'
+                  ,'<input type="checkbox" checked class="filter-checkbox" unbxdParam_facetName="{{../facet_name}}" unbxdParam_facetValue="{{value}}" id="{{../facet_name}}_{{value}}"> {{prepareFacetValue begin}} - {{prepareFacetValue end}} ({{count}})'
+                ,'</label>'
+              ,'</li>'
+            ,'{{/selected}}'
+            ,'{{#unselected}}'
+              ,'<li unbxdParam_facetName="{{../facet_name}}" unbxdParam_facetValue="{{value}}">'
+                ,'<label for="{{../facet_name}}_{{value}}">'
+                  ,'<input type="checkbox" class="filter-checkbox" unbxdParam_facetName="{{../facet_name}}" unbxdParam_facetValue="{{value}}" id="{{../facet_name}}_{{value}}"> {{prepareFacetValue begin}} - {{prepareFacetValue end}} ({{count}})'
+                ,'</label>'
+              ,'</li>'
+            ,'{{/unselected}}'
+          ,'</ul>'
+        ,'</div>'
+      ,'</div>'
+    ,'{{/rangefacets}}'
+  ].join('')
+  ,facetContainerSelector : "#facets_container"
+  ,facetCheckBoxSelector : "input[type='checkbox']"
+  ,facetElementSelector : "label"
+  ,facetOnSelect : function(el){
+      //jQuery(el).addClass('selected');
+  }
+  ,facetOnDeselect : function(el){
+      //jQuery(el).removeClass('selected');
+  }
+  ,facetMultiSelect : true
+  ,selectedFacetContainerSelector : "#selected_facets"
+  ,clearSelectedFacetsSelector : "#clear_all_selected_facets"
+  ,removeSelectedFacetSelector : ".selected-facet-delete"
+  ,selectedFacetHolderSelector : ""
+  ,loaderSelector : ""//".result-loader"
+  ,onFacetLoad : function(obj){}
+  ,sanitizeQueryString : function(q){ return q;}
+  ,getFacetStats : ""
+  ,processFacetStats : function(obj){}
+  ,setDefaultFilters : function(){}
+  ,onIntialResultLoad : function(obj){}
+  ,onPageLoad : function(obj){}
+  ,onNoResult : function(obj){}
+  ,bannerSelector: ".banner"
+  ,bannerTemp: "<a href='{{landingUrl}}'><img src='{{imageUrl}}'/></a>"
+  ,sortContainerSelector: '#sort_container'
+  ,sortOptions: [{
+    name: 'Relevancy'
+  },{
+    name: 'Price: H-L',
+    field: 'price',
+    order: 'desc'
+  },{
+    name: 'Price: L-H',
+    field: 'price',
+    order: 'asc'
+  }]
+  ,sortContainerType: 'select' /* value can be select or click */
+  ,sortContainerTemp: [
+    '<select>',
+      '{{#options}}',
+        '{{#if selected}}',
+          '<option value="{{field}}-{{order}}" unbxdsortField="{{field}}" unbxdsortValue="{{order}}" selected>{{name}}</option>',
+        '{{else}}',
+          '<option value="{{field}}-{{order}}" unbxdsortField="{{field}}" unbxdsortValue="{{order}}">{{name}}</option>',
+        '{{/if}}',
+      '{{/options}}',
+    '</select>'
+  ].join('')
+  ,paginationContainerSelector : '#pagination_container'
+  ,paginationTemp: [
+    '{{#if hasFirst}}',
+      '<span class="unbxd_first" unbxdaction="first"> &laquo; </span>',
+    '{{/if}}',
+    '{{#if hasPrev}}',
+      '<span class="unbxd_prev" unbxdaction="prev"> &lt; </span>',
+    '{{/if}}',
+    '{{#pages}}',
+      '{{#if current}}',
+        '<span class="unbxd_page highlight"> {{page}} </span>',
+      '{{else}}',
+        '<span class="unbxd_page" unbxdaction="{{page}}"> {{page}} </span>',
+      '{{/if}}',
+    '{{/pages}}',
+    '<span class="unbxd_pageof"> of </span>',
+    '<span class="unbxd_totalPages" unbxdaction="{{totalPages}}">{{totalPages}}</span>',
+    '{{#if hasNext}}',
+      '<span class="unbxd_next" unbxdaction="next"> &gt; </span>',
+    '{{/if}}',
+    '{{#if hasLast}}',
+      '<span class="unbxd_last" unbxdaction="last">&raquo;</span>',
+    '{{/if}}'
+  ].join('')
+  ,fields : ['image_url','title','brand','price','uniqueId']
+  ,searchQueryParam:"q"
+};
---
+++ karma.conf.js
@@ -0,0 +1,52 @@
+module.exports = function (config) {
+  'use strict';
+  config.set({
+
+    basePath: '',
+
+    plugins: [
+      'karma-mocha',
+      'karma-chai',
+      'karma-jquery',
+      'karma-phantomjs-launcher',
+      'karma-html2js-preprocessor',
+      'karma-sinon',
+      'karma-fixture',
+      'karma-json-fixtures-preprocessor',
+      'karma-handlebars-preprocessor',
+      'karma-verbose-reporter',
+    ],
+
+    frameworks: ['mocha', 'chai','jquery-1.8.3','sinon','fixture'],
+
+    jsonFixturesPreprocessor: {
+      variableName: '__json__'
+    },
+
+    preprocessors: {
+      '**/*.html': ['html2js'],
+      '**/*.json'   : ['json_fixtures']
+    },
+
+    files: [
+        './bower_components/handlebars/handlebars.min.js',
+        './index.html',
+        './unbxdSearch.js',
+        './app.js',
+        'mock/*.json',
+        'test/search/*.spec.js',
+    ],
+
+    reporters: ['verbose','progress'],
+
+    port: 9876,
+    colors: true,
+    autoWatch: false,
+    singleRun: true,
+
+    // level of logging
+    logLevel: config.LOG_INFO,
+
+    browsers: ['PhantomJS']
+  });
+};
