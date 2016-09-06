73bfdd4c95b173391f2919863bb71400c25cd953
santosh1994 <santosh.siddarth123@gmail.com>
2016-09-06 19:28:35 +0530

    Testcase: search Zero Results

---
+++ test/search/noResult.spec.js
@@ -0,0 +1,32 @@
+describe('No results', function () {
+  var expect = window.expect;
+  before(function(){
+    fixture.setBase('mock');
+    var zeroResultTest = fixture.load('zeroResult.json');
+    //setup document to hold search results
+    document.body.innerHTML = __html__['index.html'];
+
+    //initialize search
+    window.searchobj = new window.Unbxd.setSearch(window.config);
+
+    //stub search ajax call with mock response
+    this.stub = sinon.stub(jQuery, 'ajax').yieldsTo('success',zeroResultTest);
+    
+    this.spyOnIntialResultLoad = sinon.spy(window.searchobj.options,
+    	'onIntialResultLoad');
+    this.spyOnNoResult = sinon.spy(window.searchobj.options,'onNoResult');
+    window.searchobj.callResults(window.searchobj.paintResultSet);
+  });
+
+  after(function(){
+    this.stub.restore();
+  });
+  
+  it('Should call onNoResult',function(){
+    expect(this.spyOnNoResult.called).to.equal(true);
+  });
+
+  it('Should not call OnIntialResultLoad',function(){
+    expect(this.spyOnIntialResultLoad.called).to.equal(false);
+  });
+});
