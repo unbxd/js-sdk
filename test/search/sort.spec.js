71f2f48474b3e7691e0d91a18921374557b9ceec
santosh1994 <santosh.siddarth123@gmail.com>
2016-09-06 19:36:54 +0530

    Testcase: search Sort

---
+++ test/search/sort.spec.js
@@ -0,0 +1,53 @@
+describe('Sort', function () {
+  var expect = window.expect;
+  before(function(){
+    fixture.setBase('mock');
+    var searchTest = fixture.load('searchTestResponse.json');
+
+    //setup document to hold search results
+    document.body.innerHTML = __html__['index.html'];
+    
+    //initialize search
+    window.searchobj = new window.Unbxd.setSearch(window.config);
+
+    //stub search ajax call with mock response
+    this.stub = sinon.stub(jQuery, 'ajax').yieldsTo('success',searchTest);
+    this.spyAddSort = sinon.spy(window.searchobj,'addSort');
+  });
+
+  after(function(){
+    this.stub.restore();
+  });
+
+  beforeEach(function(){
+    //reset filters applied
+    window.searchobj.clearFilters(true);
+    window.searchobj.callResults(window.searchobj.paintResultSet);
+  });
+
+  it('Should call Addsort with field and value on which sort applied',
+    function(){
+      var field = window.searchobj.options.sortOptions[2].field;
+      var value = window.searchobj.options.sortOptions[2].order;
+      jQuery(window.searchobj.options.sortContainerSelector + ' select')
+        .prop('selectedIndex',2).change();
+      expect(this.spyAddSort.calledWith(field,value)).to.equal(true);
+    }
+  );
+
+  it('Addsort - Should add sort fields to params', function(){
+    window.searchobj.addSort('price','asc');
+    expect(window.searchobj.params.sort.price).to.be.equal('asc');
+  });
+
+  it('Removesort - Should remove sort fields from params ', function(){
+    window.searchobj.removeSort('price');
+    expect(window.searchobj.params.sort.price).to.not.exist;
+  });
+
+  it('resetSort - Should clear sort params ', function(){
+    window.searchobj.addSort('price','asc');
+    window.searchobj.resetSort();
+    expect(window.searchobj.params.sort).to.be.empty;
+  });
+});
