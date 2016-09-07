cdf5424491abe9c5b083399be1256d2f0dafd702
santosh1994 <santosh.siddarth123@gmail.com>
2016-09-07 12:14:59 +0530

    Testcase: search Banners

---
+++ test/search/banner.spec.js
@@ -0,0 +1,35 @@
+describe('Banners', function () {
+  var expect = window.expect;
+  fixture.setBase('mock');
+  var bannerResponse = fixture.load('bannerResponse.json');
+
+  before(function(){
+    //setup document to hold search results
+    document.body.innerHTML = __html__['index.html'];
+
+    //initialize search
+    window.searchobj = new window.Unbxd.setSearch(window.config);
+
+    //stub search ajax call with mock response
+    this.stub = sinon.stub(jQuery, 'ajax').yieldsTo('success',bannerResponse);
+
+  });
+
+  after(function(){
+    this.stub.restore();
+  });
+
+  beforeEach(function(){
+    window.searchobj.callResults(window.searchobj.paintResultSet);
+  });
+
+  it('Should update the Banner landingUrl', function(){
+    expect(jQuery(window.searchobj.options.bannerSelector + ' a').attr('href'))
+      .to.be.equal(bannerResponse.banner.banners[0].landingUrl);
+  });
+
+  it('Should update the Banner imageUrl', function(){
+    expect(jQuery(window.searchobj.options.bannerSelector + ' img').attr('src'))
+      .to.be.equal(bannerResponse.banner.banners[0].imageUrl);
+  });
+});
