2a9082e2c47e97ea35545b6100515bcf794958e0
Ganesh <ganesh@unbxd.com>
2019-02-25 03:07:00 +0530

    add(js-sdk-v2): folder setup

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+    entry: path.resolve(__dirname, 'app'),
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'bundle.js'
+    },
+    devServer: {
+        port: 3000,
+        contentBase: path.resolve(__dirname, 'build')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            }
+        ]
+    }
+};
